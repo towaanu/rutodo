87c1082f24244607d8032eaacb6475fc1705563c
Antoine Muller <niantoinesan@gmail.com>
2021-08-04 15:02:27 +0200

    add config

---
+++ src/config.rs
@@ -0,0 +1,42 @@
+use serde::Deserialize;
+
+#[derive(Deserialize, Debug)]
+#[serde(default)]
+pub struct AppConfig {
+    pub port: u16,
+}
+
+impl Default for AppConfig {
+    fn default() -> Self {
+        Self {
+            port: 3030,
+        }
+    }
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(default)]
+struct FlatConfig {
+    pub port: u16,
+}
+
+impl Default for FlatConfig {
+    fn default() -> Self {
+        Self {
+            port: 3030,
+        }
+    }
+}
+
+impl From<FlatConfig> for AppConfig {
+    fn from(flat_config: FlatConfig) -> Self {
+        Self {
+            port: flat_config.port,
+        }
+    }
+}
+
+pub fn get_config() -> AppConfig {
+    let flat_config: FlatConfig = envy::prefixed("RUTODO_").from_env().unwrap();
+    flat_config.into()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 use warp::Filter;
 
 mod app_logger;
+mod config;
 
 #[tokio::main]
 async fn main() {
     app_logger::init_logger();
+    let app_config = config::get_config();
     let index = warp::path::end().map(|| "Hello world !");
 
-    log::info!("Server running on port {}", 3030);
-    warp::serve(index).run(([0, 0, 0, 0], 3030)).await;
+    log::info!("Server running on port {}", app_config.port);
+    warp::serve(index).run(([0, 0, 0, 0], app_config.port)).await;
 }
